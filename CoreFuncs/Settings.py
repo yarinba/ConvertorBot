dec99f94b80ef937713e51b7068df323ef4d751e
Yarin <yarinb7877@gmail.com>
2020-10-13 01:12:37 +0300

    delete disable webhook

--- CoreFuncs/Settings.py
+++ CoreFuncs/Settings.py
@@ -7,4 +7,4 @@ JS = Myjson('res/settings.json') # Settings
 UC = Myjson('res/UserChoice.json') #User currency choice
 
 bot = telebot.TeleBot(JS.get('Token')[0])
-print(f"Delete Webhook - {bot.delete_webhook()}")
+
